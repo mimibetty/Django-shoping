46aee93dc42b86638ebc6dc794e85a95a0ecf18d
mimibetty <you@example.com>
2024-06-27 23:32:06 +0700

    insert Search function, edit display login, register when login logout

--- Django Shoping/app/views.py
+++ Django Shoping/app/views.py
@@ -17,24 +17,31 @@ def search(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else :
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
+        user_not_login = "show"
+        user_login = "hidden"
     products = Product.objects.all()
+    return render(request, 'app/search.html',{'products': products, 'cartItems': cartItems, 
+    'keys': keys , 'searched': searched, 'user_not_login': user_not_login, 'user_login': user_login})
 
-    return render(request, 'app/search.html',{'products': products, 'cartItems': cartItems, 'keys': keys , 'searched': searched})
 def register(request):
     form = CreateUserForm()
-    context = {'form' : form}
+    context = {'form' : form, 'user_not_login': 'show', 'user_login': 'hidden'}
+
     if (request.method == 'POST'):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            context = {'form' : form, 'message': 'User created successfully'}
+            context = {'form' : form, 'message': 'User created successfully', 'user_not_login': 'show', 'user_login': 'hidden'}
             return redirect('login')
         else:
-            context = {'form' : form, 'message': 'User creation failed'}
+            context = {'form' : form, 'message': 'User creation failed', 'user_not_login': 'show', 'user_login': 'hidden'}
+ 
     return render(request, 'app/register.html', context)
 
 def loginPage(request):
@@ -50,8 +57,10 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.info(request, 'Username or password is incorrect')
-            
-    context = {}
+    user_not_login = "show"
+    user_login = "hidden"
+        
+    context = { 'user_not_login': user_not_login, 'user_login': user_login}
     return render(request, 'app/login.html', context)
 
 def logoutPage(request):
@@ -64,14 +73,18 @@ def home(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else :
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
+        user_not_login = "show"
+        user_login = "hidden"
 
     products = Product.objects.all()
     # print(products)  # Thêm dòng này để kiểm tra xem products có dữ liệu hay không
-    context = {'products': products, 'cartItems': cartItems}
+    context = {'products': products, 'cartItems': cartItems, 'user_not_login': user_not_login, 'user_login': user_login}
     return render(request, 'app/home.html', context)
 
 
@@ -82,12 +95,17 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else :
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
+        user_not_login = "show"
+        user_login = "hidden"
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    context = {'items': items, 'order': order, 'cartItems': cartItems, 
+               'user_not_login': user_not_login, 'user_login': user_login}
     return render(request, 'app/cart.html', context)
 
 def checkout(request):
@@ -97,11 +115,17 @@ def checkout(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
+        user_not_login = "hidden"
+        user_login = "show"
     else :
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0}
         cartItems = order['get_cart_items']
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
+        user_not_login = "show"
+        user_login = "hidden"
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems, 
+               'user_not_login': user_not_login, 'user_login': user_login}
     return render(request, 'app/checkout.html', context)
 
 def updateItem(request):
